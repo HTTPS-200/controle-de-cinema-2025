f9de63176cb072021f897ea0421448731e2966b5
Vicente Junior <vicente_pc@outlook.com>
2025-08-22 17:26:07 -0300

    Implementado Testes de Integração ModuloFilme

--- ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
+++ ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTests.cs
@@ -1,5 +1,7 @@
 ﻿using ControleDeCinema.Dominio.ModuloFilme;
 using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Dominio.ModuloSessao;
 using ControleDeCinema.Testes.Integracao.Compartilhado;
 
 namespace ControleDeCinema.Testes.Integracao.ModuloFilme;
@@ -13,17 +15,90 @@ public sealed class RepositorioFilmeEmOrmTests : TestFixture
     {
         // Arrange
         var genero = new GeneroFilme("Ação");
-        repositorioGeneroFilme!.Cadastrar(genero);
+        repositorioGeneroFilme?.Cadastrar(genero);
         dbContext!.SaveChanges();
         var filme = new Filme("Vingadores", 120, true, genero);
 
         // Act
-        repositorioFilme!.Cadastrar(filme);
+        repositorioFilme?.Cadastrar(filme);
         dbContext.SaveChanges();
 
         // Assert
         var registro = repositorioFilme.SelecionarRegistroPorId(filme.Id);
         Assert.AreEqual(filme, registro);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Filme_Corretamente()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Ação");
+        repositorioGeneroFilme!.Cadastrar(genero);
+        dbContext!.SaveChanges();
+
+        var filme = new Filme("Vingadores", 120, true, genero);
+        repositorioFilme?.Cadastrar(filme);
+        dbContext.SaveChanges();
+
+        var filmeEditado = new Filme("Vingadores: Ultimato", 180, true, genero);
+
+        // Act
+        var editadoComSucesso = repositorioFilme?.Editar(filme.Id, filmeEditado);
+        dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioFilme?.SelecionarRegistroPorId(filme.Id);
+        Assert.IsTrue(editadoComSucesso);
+        Assert.AreEqual(filme, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Filme_Corretamente()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Ação");
+        repositorioGeneroFilme!.Cadastrar(genero);
+        dbContext!.SaveChanges();
+
+        var filme = new Filme("Vingadores", 120, true, genero);
+        repositorioFilme?.Cadastrar(filme);
+        dbContext.SaveChanges();
+
+        // Act
+        var excluidoComSucesso = repositorioFilme?.Excluir(filme.Id);
+        dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioFilme?.SelecionarRegistroPorId(filme.Id);
+        Assert.IsTrue(excluidoComSucesso);
+        Assert.IsNull(registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Filme_Corretamente()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Ação");
+        repositorioGeneroFilme!.Cadastrar(genero);
+        dbContext!.SaveChanges();
+
+        var filme1 = new Filme("Vingadores", 120, true, genero);
+        var filme2 = new Filme("Batman", 150, false, genero);
+        var filme3 = new Filme("Spider-Man", 130, false, genero);
+
+        repositorioFilme?.Cadastrar(filme1);
+        repositorioFilme?.Cadastrar(filme2);
+        repositorioFilme?.Cadastrar(filme3);
+        dbContext.SaveChanges();
+
+        List<Filme> listaFilmes = [filme1, filme2, filme3];
+        var listFilmesOrdenados = listaFilmes.OrderBy(f => f.Titulo).ToList();
+
+        // Act
+        var registros = repositorioFilme?.SelecionarRegistros().OrderBy(f => f.Titulo).ToList();
+
+        // Assert
+        CollectionAssert.Equals(listFilmesOrdenados, registros);
     }
 
 }
