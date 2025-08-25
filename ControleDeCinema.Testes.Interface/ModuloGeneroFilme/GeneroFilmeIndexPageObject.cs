22ff694d25c4cc9c3bfa6b1f53e68dfa9494217d
Vicente Junior <vicente_pc@outlook.com>
2025-08-25 16:31:03 -0300

    Implementado Projeto Testes-Interface Implementado TestFixture. e MSTestSettings

---
+++ ControleDeCinema.Testes.Interface/ModuloGeneroFilme/GeneroFilmeIndexPageObject.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+internal class GeneroFilmeIndexPageObject
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+
+    public GeneroFilmeIndexPageObject(IWebDriver driver)
+    {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+
+    public GeneroFilmeIndexPageObject IrPara(string enderecoBase)
+    {
+        driver.Navigate().GoToUrl($"{enderecoBase}/GeneroFilme");
+        return this;
+    }
+}
---
+++ ControleDeCinema.Testes.Interface/ModuloGeneroFilme/GeneroFilmeIntefaceTests.cs
@@ -0,0 +1,15 @@
+﻿using ControleDeCinema.Testes.Interface.Compartilhado;
+using OpenQA.Selenium;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+
+[TestClass]
+[TestCategory("Tests de Interface de GeneroFilme")]
+public sealed class GeneroFilmeIntefaceTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_GeneroFilme_Corretamente()
+    {
+
+    }
+}
