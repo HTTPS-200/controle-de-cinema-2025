d8b86845c3151cb75e91f68e2a279b3161d51dcf
Vicente Junior <vicente_pc@outlook.com>
2025-08-26 15:22:26 -0300

    configuração do dockerfile

---
+++ ControleDeCinema.Testes.Interface/ModuloGeneroFilme/GeneroFilmeFormObjects.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+public class GeneroFilmeFormObjects
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public GeneroFilmeFormObjects(IWebDriver driver)
+    {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        wait.Until(d => d.FindElement(By.CssSelector("form")).Displayed);
+    }
+}
